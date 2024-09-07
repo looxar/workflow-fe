06ac39de321c80d0ca7361a008a5abda8f3325a5
Panuwit <looxar.wicher@gmail.com>
2024-09-07 12:37:36 +0700

    react day3 execise3

--- src/component/greeting.ts
+++ src/component/greeting.ts
@@ -5,15 +5,24 @@ interface GreetingParams {
   format?: 'Table' | 'Default';
 }
 
+function funcHi(task) {
+  console.table({ name: task.name, age: task.age });
+  console.log('Hi ', task.name);
+}
+
 function greeting(params: GreetingParams) {
   const task = {
     name: params.name,
     age: params.age,
-    format: params.format ? params.format : 'Default',
+    format: params.format ? params.format : 'funcHi',
   };
   // render
   // return <ObjectRender name={task.name} />
-  console.table({ name: task.name, age: task.age, format: task.format });
+  if (task.format == 'funcHi') {
+    funcHi(task);
+  } else {
+    console.table({ name: task.name, age: task.age, format: task.format });
+  }
   // console.log(task.name);
   // console.log(task.age);
   // console.log(task.format);
@@ -29,4 +38,4 @@ greeting({ name: 'Bob', age: 18, format: 'Default' });
 
 greeting({ name: 'Charlie', age: 18 });
 
-greeting({ name: 'Bob', age: 18, format: 'abc' });
+// greeting({ name: 'Bob', age: 18, format: 'abc' });
